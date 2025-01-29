45cddc5ce649de35878432736f2442031b4aab4d
Manaregr8 <manjeetdevelops@gmail.com>
2025-01-29 22:55:16 +0530

    changed youtube

--- src/app/api/youtube/route.js
+++ src/app/api/youtube/route.js
@@ -1,7 +1,7 @@
 // src/app/api/youtube/route.js
 
 export async function GET(req, res) {
-  const API_KEY = "AIzaSyAncuw1yKqe5tb8rTMHX3qNAKIblKoxoAI"; // Your API key
+  const API_KEY = "AIzaSyCszLgJ9s8XHUeruLhURmTLxDxDg5jN6SY";
   const CHANNEL_ID = "UCz6g2YiUmSeZVg-QxB-lKSQ"; // Replace with your YouTube channel ID
   const BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 
