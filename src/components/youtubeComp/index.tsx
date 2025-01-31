a1844da98da6dcb47b7594000b1d926e4972ba2e
Manaregr8 <manjeetdevelops@gmail.com>
2025-01-31 17:13:18 +0530

    added youtube

--- src/components/youtubeComp/index.tsx
+++ src/components/youtubeComp/index.tsx
@@ -1,16 +1,8 @@
-"use client";
-import { useEffect } from "react";
 import "./youtube.css"
 const ElfsightYouTube = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://static.elfsight.com/platform/platform.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
 
   return (
-    <div className="cleanContainerYoutube elfsight-app-4ea9ca53-6e5c-4d7d-b58b-5736cf58a3f1" data-elfsight-app-lazy></div>
+    <iframe className="cleanContainerYoutube" src='https://widgets.sociablekit.com/youtube-channel-videos/iframe/25517770'></iframe>
   );
 };
 
